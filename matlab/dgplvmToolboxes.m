a46ccb08a3c9563e8760540c83b582b46d95abcd
lawrennd <lawrennd@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2013-11-24 20:13:32 +0000

    General update commit

--- matlab/dgplvmToolboxes.m
+++ matlab/dgplvmToolboxes.m
@@ -1,12 +1,4 @@
 % DGPLVMTOOLBOXES Load in the relevant toolboxes for dgplvm.
 
 importLatest('netlab');
-importLatest('prior');
-importLatest('optimi');
-importLatest('datasets');
-importLatest('mltools');
-importLatest('kern');
-importLatest('ndlutil');
-importLatest('noise');
-importLatest('gp');
-importLatest('fgplvm');
+importLatest('GPmat');
